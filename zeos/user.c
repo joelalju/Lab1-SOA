f54e0228e5da240333a81d69652b8dbc25552075
Joel Alarcón <joelalju100@gmail.com>
2018-02-20 15:45:13 +0100

    Add files via upload

---
+++ zeos/user.c
@@ -0,0 +1,40 @@
+#include <libc.h>
+
+char buff[24];
+
+int pid;
+
+long inner(long n) {
+	int i;
+	long suma;
+	suma = 0;
+	for (i = 0; i<n; i++) suma = suma + i;
+	return suma;	
+}
+long outer (long n) {
+	int i;
+	long acum;
+	acum = 0;
+	for (i=0;i<n;i++) acum = acum + inner(i);
+	return acum;
+}
+
+int add (int par1, int par2) {
+	int par = par1+par2;
+	return par;
+}
+
+int __attribute__ ((__section__(".text.main")))
+  main(void)
+{
+    /* Next line, tries to move value 0 to CR3 register. This register is a privileged one, and so it will raise an exception */
+     /* __asm__ __volatile__ ("mov %0, %%cr3"::"r" (0) ); */
+	long count, acum;
+	count = 75;
+	acum = 0;
+	acum = outer(count);
+	while(1) {
+		acum = add(50,26);
+	}
+	return 0;
+}
